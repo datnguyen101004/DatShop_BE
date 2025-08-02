f2089ac95863a5db589a56f3b845a1d971644797
Đạt <datnguyen101004@gmail.com>
2025-08-03 00:47:27 +0700

    fix user controller

--- src/main/java/com/dat/backend/datshop/user/controller/UserController.java
+++ src/main/java/com/dat/backend/datshop/user/controller/UserController.java
@@ -7,6 +7,7 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,6 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1/user")
 @RequiredArgsConstructor
 @Slf4j
+@PreAuthorize("hasRole('ADMIN') or hasRole('USER')")
 public class UserController {
     private final UserService userService;
 
